a4c558dd671c10b784bcb4afca1d77945c60de50
ernestoadriano <140088177+ernestoadriano@users.noreply.github.com>
2024-12-08 19:15:55 +0200

    Update

---
+++ backend/loja-virtual/src/main/java/com/eddy/loja_virtual/service/EmailService.java
@@ -0,0 +1,32 @@
+package com.eddy.loja_virtual.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+/*
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String remetente;
+
+    public String sendEmailText(String destiny, String subject, String message) {
+
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setFrom("edyadriano65@gmail.com");
+            simpleMailMessage.setTo(destiny);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(message);
+            javaMailSender.send(simpleMailMessage);
+            return "Email enviado";
+        } catch (Exception e) {
+            return "Erro ao tentar enviar email";
+        }
+    }*/
+}
